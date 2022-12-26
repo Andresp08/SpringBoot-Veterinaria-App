0b28097acf8179d3436874b88b262233b80390d3
unknown <andresfp29032001@gmail.com>
2022-12-26 10:42:54 -0500

    starting a new project

---
+++ src/main/java/com/co/andresfot/veterinaria/SpringBootVeterinariaAppApplication.java
@@ -0,0 +1,13 @@
+package com.co.andresfot.veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVeterinariaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVeterinariaAppApplication.class, args);
+	}
+
+}
