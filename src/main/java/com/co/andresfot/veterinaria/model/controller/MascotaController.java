f16e428a94162f452c42da818668602ad23db2af
unknown <andresfp29032001@gmail.com>
2023-01-01 12:39:38 -0500

    working with mascota file image upload and mascota details

--- src/main/java/com/co/andresfot/veterinaria/model/controller/MascotaController.java
+++ src/main/java/com/co/andresfot/veterinaria/model/controller/MascotaController.java
@@ -1,10 +1,15 @@
 package com.co.andresfot.veterinaria.model.controller;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
 
+import org.springframework.core.io.Resource;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,12 +20,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.co.andresfot.veterinaria.model.entity.Mascota;
 import com.co.andresfot.veterinaria.model.entity.Propietario;
 import com.co.andresfot.veterinaria.model.service.IMascotaService;
 import com.co.andresfot.veterinaria.model.service.IPropietarioService;
+import com.co.andresfot.veterinaria.model.service.IUploadFileService;
 
 @Controller
 @RequestMapping("/mascotas")
@@ -32,6 +39,9 @@ public class MascotaController {
 
 	@Autowired
 	private IPropietarioService propietarioService;
+	
+	@Autowired
+	private IUploadFileService uploadFileService;
 
 	@GetMapping("/listado-mascotas")
 	public String listadoMascotas(Model model) {
@@ -55,8 +65,10 @@ public class MascotaController {
 	}
 	
 	@PostMapping("/nueva-mascota")
-	public String guardarMascota(@Valid Mascota mascota, BindingResult result, Model model,  
-			@RequestParam(name = "propietario") Long id, RedirectAttributes flash, SessionStatus status) {
+	public String guardarMascota(@Valid Mascota mascota, BindingResult result, Model model,
+			@RequestParam(name = "file") MultipartFile foto,
+			@RequestParam(name = "propietario") Long id,
+			RedirectAttributes flash, SessionStatus status) {
 		
 		if(result.hasErrors()) {
 			List<Propietario> propietarios = propietarioService.findAllPropietarios();
@@ -64,6 +76,24 @@ public class MascotaController {
 			model.addAttribute("propietarios", propietarios);
 			model.addAttribute("mascota", mascota);
 			return "mascotas/nueva-mascota";
+		}
+		
+		if(!foto.isEmpty()) {
+			if(mascota.getId() !=null && mascota.getId() > 0 && mascota.getFoto() != null
+					&& mascota.getFoto().length() > 0) {
+				uploadFileService.delete(mascota.getFoto());
+			}
+			
+			String uniqueFileName = null;
+			
+			try {
+				uniqueFileName = uploadFileService.copy(foto);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			flash.addFlashAttribute("info", "Has subido correctamente '" + uniqueFileName + "'");
+			mascota.setFoto(uniqueFileName);
 		}
 		
 		Propietario propietario = propietarioService.findPropietarioById(id);
@@ -74,6 +104,22 @@ public class MascotaController {
 		
 		
 		return "redirect:/mascotas/listado-mascotas";
+	}
+	
+	@GetMapping("/uploads/{filename:.+}")
+	public ResponseEntity<Resource> obtenerImagenMascota(@PathVariable String filename){
+		
+		Resource recurso = null;
+		
+		try {
+			recurso = uploadFileService.load(filename);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"")
+				.body(recurso);
 	}
 	
 	@GetMapping("/editar-mascota/{id}")
@@ -121,6 +167,17 @@ public class MascotaController {
 		mascotaService.deleteMascotaById(mascota.getId());
 		
 		return "redirect:/mascotas/listado-mascotas";
+	}
+	
+	@GetMapping("/detalle-mascota/{id}")
+	public String verDetalleMascota(@PathVariable Long id, Model model) {
+		
+		Mascota mascota = mascotaService.findMascotaById(id);
+		
+		model.addAttribute("mascota", mascota);
+		model.addAttribute("titulo", "Detalle mascota " + id);
+		
+		return "mascotas/detalle-mascota";
 	}
 
 }
